5956f4648f05bd8ea0f20b3b9ed7e1b94ace87f0
Vladislav <mahkamovvlad06@gmail.com>
2026-09-02 20:09:00 +0600

    TASK-1003:Настроил PostgreSQL и миграции

--- internal/app/config/section/repository.go
+++ internal/app/config/section/repository.go
@@ -4,15 +4,18 @@ import "time"
 
 type (
 	Repository struct {
-		Postgres RepositoryPostgres
+		Postgres       RepositoryPostgres
+		MigrationTable string `split_words:"true" default:"schema_migrations"`
 	}
 
 	RepositoryPostgres struct {
-		Address      string        `required:"true"`
-		Username     string        `required:"true"`
-		Password     string        `required:"true"`
-		Name         string        `required:"true"`
-		ReadTimeout  time.Duration `split_words:"true" default:"30s"`
-		WriteTimeout time.Duration `split_words:"true" default:"30s"`
+		Address        string        `required:"true"`
+		Username       string        `required:"true"`
+		Password       string        `required:"true"`
+		Name           string        `required:"true"`
+		ConnTimeout    time.Duration `split_words:"true" default:"10s"` // Добавили таймаут подключения!
+		ReadTimeout    time.Duration `split_words:"true" default:"30s"`
+		WriteTimeout   time.Duration `split_words:"true" default:"30s"`
+		MigrationTable string        `split_words:"true" default:"schema_migrations"` // Добавили таблицу миграций для TASK-1003!
 	}
 )
